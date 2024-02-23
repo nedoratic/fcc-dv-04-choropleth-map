11ef31525b59e15be9b31c7fa5a2630327ee1bbe
nedoratic <nedoratic@gmail.com>
2024-02-23 21:16:06 +0100

    index.js - file created and basic variables added

---
+++ index.js
@@ -0,0 +1,7 @@
+let countyURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json';
+let educationURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json';
+
+let countyData;
+let educationData;
+
+let canvas = d3.select('#canvas');
